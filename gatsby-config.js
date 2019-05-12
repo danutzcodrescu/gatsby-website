f6b0639bfac9844dfa322fc9c324956c3ff255ee
danutzcodrescu <danutzcodrescu@gmail>
2019-05-12 23:04:17 +0200

    chore 🛠️: switch ts version to run smoothly in vscode, add styled components plugin for build

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-eslint`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
