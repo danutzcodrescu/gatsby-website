d02433b884acca21a339fb194bf891142c0436c5
danutzcodrescu <danutzcodrescu@gmail>
2019-05-17 23:46:26 +0200

    fix 🔥️: layout of the page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -20,8 +20,17 @@ interface Props {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   height: 100vh;
+
+  @media (max-width: 756px) {
+    height: auto;
+  }
+`;
+const Main = styled.main`
+  min-height: calc(100vh - 160px);
+  @media (max-width: 756px) {
+    min-height: none;
+  }
 `;
 
 const Layout = ({ children }: Props) => (
@@ -39,17 +48,8 @@ const Layout = ({ children }: Props) => (
       <ThemeProvider theme={theme}>
         <Container>
           <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-              maxHeight: 'calc(100vh - 160px)',
-            }}
-          >
-            <main>{children}</main>
-          </div>
+
+          <Main>{children}</Main>
           <Footer />
         </Container>
       </ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,16 @@ import SEO from '../components/seo';
 const Container = styled.div`
   color: white;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-wrap: wrap;
+  text-align: center;
+  align-items: center;
+  height: 100%;
+  > * {
+    width: 100%;
+  }
   h1 {
     margin: 1rem;
     font-size: 2rem;
-    width: 100%;
-    text-align: center;
   }
 `;
 
@@ -24,13 +26,15 @@ const IndexPage = () => (
     {/* <Image /> */}
     {/* </div> */}
     <Container>
-      <h1>Danut Codrescu</h1>
+      <div>
+        <h1>Danut Codrescu</h1>
 
-      <h3>
-        Fullstack developer <i className="fas fa-laptop-code" /> | Javascript
-        addict <i className="fab fa-js-square" /> | Sports enthusiast{' '}
-        <i className="fas fa-running" />
-      </h3>
+        <h3>
+          Fullstack developer <i className="fas fa-laptop-code" /> | Javascript
+          addict <i className="fab fa-js-square" /> | Sports enthusiast{' '}
+          <i className="fas fa-running" />
+        </h3>
+      </div>
     </Container>
   </Layout>
 );
